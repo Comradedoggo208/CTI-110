4fb3c5f8d27311a3d3adfa6cb84c7f3d40a60978
Comradedoggo208 <serpat6966@student.faytechcc.edu>
2024-09-24 14:21:17 -0400

    First upload

---
+++ P1LAB1_SerpaTeofilo.py
@@ -0,0 +1,13 @@
+# Teofilo Serpa
+# 9/24/24
+# P1 LAB1
+# It shows how to get input from the user and display output.
+
+# Have user enter first name here
+first_name = input("enter first name: ")
+
+# Also get last name from user
+last_name = input("enter last name: ")
+
+# display info to the user
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
