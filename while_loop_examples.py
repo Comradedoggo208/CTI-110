5bf639e7bc8bc02d0c86f6d2eecc61248facd105
Comradedoggo208 <serpat6966@student.faytechcc.edu>
2024-11-07 14:40:44 -0500

    While_Loop_examples

---
+++ while_loop_examples.py
@@ -0,0 +1,18 @@
+#while loops examples
+
+'''
+run_again =  input(f"Enter 'run' to use the program or 'exit' to end: ")
+
+while run_again != "exit":
+    print("Do something...")
+    run_again = input(f"put 'enter' to run, or 'exit' to stop: ")
+# loop breaks here
+print("thanks for using the program... BUH BY!")
+'''
+
+num = 0
+
+while num < 5:
+    print(";)" * 10)
+    num += 1
+print("THE AGE OF LOOPS HAS ENDED!")
