d8bec9248abd0a33a32417aab972314b2cf1b600
Comradedoggo208 <serpat6966@student.faytechcc.edu>
2024-11-21 13:15:21 -0500

    Update Intro_to_functions_python_TeofiloSerpa.py

--- Intro_to_functions_python_TeofiloSerpa.py
+++ Intro_to_functions_python_TeofiloSerpa.py
@@ -3,7 +3,7 @@
 # define a  function that adds numbers
 def add_num(num1, num2, num3):
     result = num1 + num2 + num3
-    print(f"your result is {result}!")
+    return result
 
 
 
@@ -13,7 +13,10 @@ def main():
     print("and this...This is a main function")
     
     # call new function
-    add_num(5,8,3)
+   result1 = add_num(5,8,3)
+    print(result1)
+    print("adding the result1 variable to some 0's")
+    print(add_num(result1,1,1))
     
 # call the main function
 if __name__ == "__main__":
